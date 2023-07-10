fb489a43e68de75f5c1bdc64d79533ef13644657
Mov305 <abd3052001@gmail.com>
2023-07-10 18:16:18 +0300

    Added most of the functions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import '@hotwired/turbo-rails';
+import './controllers';
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './react/index';
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'root/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "root/index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # The route of greetings API is /api/v1/greetings
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
+
+  root "root#index"
 end
